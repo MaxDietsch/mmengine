af8b8d4ffbf41deb5539529e79963c727bf9d679
Max <diedietschies@gmail.com>
2024-02-18 12:54:16 +0100

    CoSen test

--- mmengine/runner/loops.py
+++ mmengine/runner/loops.py
@@ -535,9 +535,7 @@ class CoSenTrainLoop(BaseLoop):
                         data_samples = batch['data_samples']
                         labels = torch.cat([i.gt_label for i in data_samples])
                         outs = self.runner.model.extract_feat(inputs)
-                        print(outs)
-                        print(outs[0].shape)
-                        [self.v[label].append(outs[ix]) for ix, label in enumerate(labels) if len(self.v[label]) < self.s_samples_per_class[label]]
+                        [self.v[label].append(outs[0][ix]) for ix, label in enumerate(labels) if len(self.v[label]) < self.s_samples_per_class[label]]
                         
                         if all( x > y for x, y in zip([len(self.v[i]) for i in range(self.num_classes)], self.s_samples_per_class)):
                             break
