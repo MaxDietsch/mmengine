bfe042fd05e218a698b68e00d92bdae509cb65a6
diedietschies <diedietschies@gmail.com>
2024-02-19 13:03:44 +0100

    cosen test

--- mmengine/runner/loops.py
+++ mmengine/runner/loops.py
@@ -655,6 +655,7 @@ class CoSenTrainLoop(BaseLoop):
         print(self.y_pred.shape)
 
         batch_size = self.dataloader.batch_size
+        print(idx)
         self.y_pred[(idx -1) * batch_size : idx * batch_size] = pred_labels
         print(self.y_pred)
 
