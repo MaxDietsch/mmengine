2d0f550854568d1dd83c7a345e78ef92dd7c1aab
diedietschies <diedietschies@gmail.com>
2024-02-20 08:34:05 +0100

    cosen test

--- mmengine/runner/loops.py
+++ mmengine/runner/loops.py
@@ -631,7 +631,10 @@ class CoSenTrainLoop(BaseLoop):
 
                     t_temp = torch.mul(torch.exp( - (self.c2c_sep - self.mu1) ** 2 / (2 * self.s1 ** 2)), torch.exp( - (r - self.mu2) ** 2 / (2 * self.s2 ** 2)))
                     t = torch.mul(self.h, t_temp)
-                    print(t)
+
+                    print(torch.exp( - (self.c2c_sep - self.mu1) ** 2 / (2 * self.s1 ** 2)))
+                    print(torch.exp( - (r - self.mu2) ** 2 / (2 * self.s2 ** 2)))
+                    print(self.h)
 
                     grad = self.runner.model.head.loss_module.compute_grad(t.view(-1, 1))
                     print(grad)
