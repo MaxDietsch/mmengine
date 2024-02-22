5f85bcd043ede24844051c6b7a99397d524f24a8
Max <diedietschies@gmail.com>
2024-02-22 08:23:58 +0100

    crl test

--- mmengine/runner/__init__.py
+++ mmengine/runner/__init__.py
@@ -9,7 +9,7 @@ from .checkpoint import (CheckpointLoader, find_latest_checkpoint,
                          get_torchvision_models, load_checkpoint,
                          load_state_dict, save_checkpoint, weights_to_cpu)
 from .log_processor import LogProcessor
-from .loops import EpochBasedTrainLoop, IterBasedTrainLoop, TestLoop, ValLoop, DOSTrainLoop, CoSenTrainLoop
+from .loops import EpochBasedTrainLoop, IterBasedTrainLoop, TestLoop, ValLoop, DOSTrainLoop, CoSenTrainLoop, HardSamplingBasedTrainLoop
 from .priority import Priority, get_priority
 from .runner import Runner
 from .utils import set_random_seed
@@ -22,4 +22,5 @@ __all__ = [
     'TestLoop', 'Runner', 'get_priority', 'Priority', 'find_latest_checkpoint',
     'autocast', 'LogProcessor', 'set_random_seed', 'FlexibleRunner',
     'turn_on_activation_checkpointing', 'DOSTrainLoop', 'CoSenTrainLoop', 
+    'HardSamplingBasedTrainLoop', 
 ]
--- mmengine/runner/loops.py
+++ mmengine/runner/loops.py
@@ -15,8 +15,10 @@ from .utils import calc_dynamic_intervals
 
 # for usage of self made sampler
 import numpy as np
+# only EpochBasedTrainLoop works with DynamicSampler
 from mmengine.dataset import DynamicSampler
 import torch.nn.functional as F
+
 
 @LOOPS.register_module()
 class EpochBasedTrainLoop(BaseLoop):
@@ -736,6 +738,170 @@ class CoSenTrainLoop(BaseLoop):
         step = bisect.bisect(self.dynamic_milestones, (self.epoch + 1))
         self.val_interval = self.dynamic_intervals[step - 1]
 
+
+@LOOPS.register_module()
+class HardSamplingBasedTrainLoop(BaseLoop):
+    """Loop based on epoch-based training based on: 
+        Imbalanced Deep Learning by Minority Class Incremental Rectification by Qi Dong et al.
+        (only class based sampling with relative comparison)
+        Requirements: 
+            new (non-default parameters in the init)
+
+    Args:
+        runner (Runner): A reference of runner.
+        dataloader (Dataloader or dict): A dataloader object or a dict to
+            build a dataloader.
+        max_epochs (int): Total training epochs.
+        min_classes List(int): Determines the minority classes for the algorithm (only
+            these will be used for mining hard samples, in the paper there is a criterion for that)
+        val_begin (int): The epoch that begins validating.
+            Defaults to 1.
+        val_interval (int): Validation interval. Defaults to 1.
+        dynamic_intervals (List[Tuple[int, int]], optional): The
+            first element in the tuple is a milestone and the second
+            element is a interval. The interval is used after the
+            corresponding milestone. Defaults to None.
+    """
+
+    def __init__(
+            self,
+            runner,
+            dataloader: Union[DataLoader, Dict],
+            max_epochs: int,
+            min_classes: List[int], 
+            val_begin: int = 1,
+            val_interval: int = 1,
+            dynamic_intervals: Optional[List[Tuple[int, int]]] = None) -> None:
+        super().__init__(runner, dataloader)
+        self._max_epochs = int(max_epochs)
+        assert self._max_epochs == max_epochs, \
+            f'`max_epochs` should be a integer number, but get {max_epochs}.'
+        self._max_iters = self._max_epochs * len(self.dataloader)
+        self._epoch = 0
+        self._iter = 0
+        self.val_begin = val_begin
+        self.val_interval = val_interval
+        # This attribute will be updated by `EarlyStoppingHook`
+        # when it is enabled.
+        self.stop_training = False
+        if hasattr(self.dataloader.dataset, 'metainfo'):
+            self.runner.visualizer.dataset_meta = \
+                self.dataloader.dataset.metainfo
+        else:
+            print_log(
+                f'Dataset {self.dataloader.dataset.__class__.__name__} has no '
+                'metainfo. ``dataset_meta`` in visualizer will be '
+                'None.',
+                logger='current',
+                level=logging.WARNING)
+
+        self.dynamic_milestones, self.dynamic_intervals = \
+            calc_dynamic_intervals(
+                self.val_interval, dynamic_intervals)
+
+        # for Hard Sampling 
+        self.num_classes = len(self.dataloader.dataset.metainfo['classes'])
+
+        # used to say which classes should be used for hard sampling
+        self.min_classes = min_classes
+
+        # safe hard samples, first dim is hard neg (0) or hard pos (1)
+        # second dim is class of hard sample
+        self.hard_samples = [[[] for _ in range(self.num_classes)] for _ in range(2)] #torch.empty((self.num_classes, ))
+
+    @property
+    def max_epochs(self):
+        """int: Total epochs to train model."""
+        return self._max_epochs
+
+    @property
+    def max_iters(self):
+        """int: Total iterations to train model."""
+        return self._max_iters
+
+    @property
+    def epoch(self):
+        """int: Current epoch."""
+        return self._epoch
+
+    @property
+    def iter(self):
+        """int: Current iteration."""
+        return self._iter
+
+    def mine_hard_samples(self):
+        for idx, data_batch in enumerate(self.dataloader):
+            batch = self.runner.model.data_preprocessor(data_batch, True)
+            inputs = batch['inputs']
+            data_samples = batch['data_samples']
+            labels = torch.cat([i.gt_label for i in data_samples])
+            pred = self.runner.model.predict(inputs)
+            print(pred)
+
+
+
+
+
+
+    def run(self) -> torch.nn.Module:
+        """Launch training."""
+        self.runner.call_hook('before_train')
+        
+
+        while self._epoch < self._max_epochs and not self.stop_training:
+            
+            self.mine_hard_samples()
+
+            self.run_epoch()
+            self._decide_current_val_interval()
+            if (self.runner.val_loop is not None
+                    and self._epoch >= self.val_begin
+                    and self._epoch % self.val_interval == 0):
+
+        
+        self.runner.call_hook('after_train')
+        return self.runner.model
+
+    def run_epoch(self) -> None:
+        """Iterate one epoch."""
+        self.runner.call_hook('before_train_epoch')
+        self.runner.model.train()
+
+        for idx, data_batch in enumerate(self.dataloader):
+            # only run iterations for classes that should be included 
+            # if data_batch['data_samples'][0].gt_label.item() in cls:
+                # self.run_iter(idx, data_batch)
+            #print(data_batch)
+            self.run_iter(idx, data_batch)
+
+        self.runner.call_hook('after_train_epoch')
+        self._epoch += 1
+
+    def run_iter(self, idx, data_batch: Sequence[dict]) -> None:
+        """Iterate one min-batch.
+
+        Args:
+            data_batch (Sequence[dict]): Batch of data from dataloader.
+        """
+        self.runner.call_hook(
+            'before_train_iter', batch_idx=idx, data_batch=data_batch)
+        # Enable gradient accumulation mode and avoid unnecessary gradient
+        # synchronization during gradient accumulation process.
+        # outputs should be a dict of loss.
+        outputs = self.runner.model.train_step(
+            data_batch, optim_wrapper=self.runner.optim_wrapper)
+
+        self.runner.call_hook(
+            'after_train_iter',
+            batch_idx=idx,
+            data_batch=data_batch,
+            outputs=outputs)
+        self._iter += 1
+
+    def _decide_current_val_interval(self) -> None:
+        """Dynamically modify the ``val_interval``."""
+        step = bisect.bisect(self.dynamic_milestones, (self.epoch + 1))
+        self.val_interval = self.dynamic_intervals[step - 1]
 
 
 @LOOPS.register_module()
