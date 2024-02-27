6646dde5e16804bc558d4efa8973f0e8b22a92e0
Max Dietsch <diedietschies@gmail.com>
2024-02-27 08:40:08 +0100

    crl test

--- mmengine/runner/loops.py
+++ mmengine/runner/loops.py
@@ -975,7 +975,7 @@ class HardSamplingBasedTrainLoop(BaseLoop):
                             print(k[2].cpu())
                             
                             data_batch['data_samples'] = None 
-                            data_batch['inputs'] = data_batch['inputs'][1].unsqueeze(0)
+                            data_batch['inputs'] = data_batch['inputs'][0].unsqueeze(0)
 
                             print(data_batch)
                             data_batch = self.runner.model.data_preprocessor(data_batch, True)
