a2a6454a9ba818726aab853a6a8ec4db14e9f7cd
Max <diedietschies@gmail.com>
2024-03-11 17:10:37 +0100

    pytorchifying dos

--- mmengine/runner/loops.py
+++ mmengine/runner/loops.py
@@ -434,7 +434,6 @@ class DOSTrainLoop(BaseLoop):
             indices = [torch.topk(self.d[i][j], self.k[i], largest = False).indices for j in range(self.samples_per_class[i])]
             indices = torch.stack(indices, dim = 0)
             #print(indices)
-            print(self.v[i][0].device)
             n = self.v[i][indices]
             #print(n)
             #print(n.shape)
