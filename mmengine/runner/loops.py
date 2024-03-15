709a2d0704b8cc637d8a59cb958f56966f495e29
diedietschies <diedietschies@gmail.com>
2024-03-15 13:31:29 +0100

    dos

--- mmengine/runner/loops.py
+++ mmengine/runner/loops.py
@@ -422,7 +422,7 @@ class DOSTrainLoop(BaseLoop):
 
         # get mutual distance matrix
         self.calc_mutual_distance_matrix()
-        print(self.d)
+        #print(self.d)
 
         for i in range(self.num_classes):
 
@@ -432,7 +432,7 @@ class DOSTrainLoop(BaseLoop):
             
             indices = [torch.topk(self.d[i][j], self.k[i], largest = False).indices for j in range(self.samples_per_class[i])]
             indices = torch.stack(indices, dim = 0)
-            #print(indices)
+            print(indices)
             n = self.v[i][indices]
             #print(n)
             #print(n.shape)
