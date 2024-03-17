b8897f2f7813018ff2a74624f6c07dea181e8cd6
diedietschies <diedietschies@gmail.com>
2024-03-17 13:10:52 +0100

    dos

--- mmengine/runner/loops.py
+++ mmengine/runner/loops.py
@@ -502,8 +502,9 @@ class DOSTrainLoop(BaseLoop):
             assert len(n_p[0]) == len(n_n[0]), 'first length not identical'
             for j in range(len(n_p[0])):
                 assert len(n_p[0][0]) == len(n_n[0][0]), 'second length not identical'
+                print(len(n_p[0][0]]))
                 for l in range(len(n_p[0][0])):
-                    #print(n_p[i][j][l])
+                    print(n_p[i][j][l])
                     print(n_n[i][j][l])
                     print(n_p[i][j][l] == n_n[i][j][l])
                     
