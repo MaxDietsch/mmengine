4fcdc0b9a24ae33c245b03a9847ac87e2d75a556
diedietschies <diedietschies@gmail.com>
2024-03-17 12:31:47 +0100

    dos

--- mmengine/runner/loops.py
+++ mmengine/runner/loops.py
@@ -481,7 +481,7 @@ class DOSTrainLoop(BaseLoop):
                 # get deep features with shortest distance to feature vector with batch index of batch_idx[i][j]
                 for x in torch.topk(self.d[i][j], self.k[i], largest = False).indices:
                     n.append(self.v[i][x])
-                test_n.append(n)
+                test_n.append(torch.tensor(n))
                 
                 # sample weight vectors
                 w = (torch.abs(torch.randn(self.r[i], self.k[i]))).to(torch.device("cuda"))
