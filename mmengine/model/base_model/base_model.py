197a2392f09a66cd40d02c58474703c211977349
Max Dietsch <diedietschies@gmail.com>
2024-03-20 15:04:39 +0100

    balanced mixup

--- mmengine/model/base_model/base_model.py
+++ mmengine/model/base_model/base_model.py
@@ -110,9 +110,7 @@ class BaseModel(BaseModule):
         """
         # Enable automatic mixed precision training context.
         with optim_wrapper.optim_context(self):
-            print(data)
             data = self.data_preprocessor(data, True)
-            print(data)
             losses = self._run_forward(data, mode='loss')  # type: ignore
         parsed_losses, log_vars = self.parse_losses(losses)  # type: ignore
         optim_wrapper.update_params(parsed_losses)
